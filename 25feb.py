29c196b55b9de5b3ec2a4e33c7c5b501eae94fed
abhigna333 <abhignareddy333@gmail.com>
2023-02-25 11:20:10 +0530

    n/mth salary problem

---
+++ 25feb.py
@@ -0,0 +1,10 @@
+print("Enter a/bth of salary is M")
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+M = int(input("Enter M: "))
+
+print("Enter c/dth of salary needed ")
+c = int(input("Enter c: "))
+d = int(input("Enter d: "))
+res = (M * c * b) / (a * d)
+print(f"{c}/{d}th of the salary is {res}")
