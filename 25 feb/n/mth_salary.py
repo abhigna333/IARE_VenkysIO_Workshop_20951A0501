07f930626ba97440aacf26a3a41e80d473f7d3c6
abhigna333 <abhignareddy333@gmail.com>
2023-02-25 11:55:26 +0530

    Changed folder structure

--- 25 feb/n/mth_salary.py
+++ 25 feb/n/mth_salary.py
@@ -1,3 +1,5 @@
+#n/m th salary
+
 print("Enter a/bth of salary is M")
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
@@ -8,3 +10,6 @@ c = int(input("Enter c: "))
 d = int(input("Enter d: "))
 res = (M * c * b) / (a * d)
 print(f"{c}/{d}th of the salary is {res}")
+
+
+#Reverse of a string
